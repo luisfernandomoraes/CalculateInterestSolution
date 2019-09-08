8d5c5af45458e310a9fabcaea275731b9c9eeb18
Luís Fernando Moraes <luisfernando.pereira.moraes@gmail.com>
2019-09-08 12:27:25 -0300

    adicionando testes de integração

---
+++ Softplan.CalcTest.CalculateInterestApi/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Softplan.CalcTest.CalculateInterestApi.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("/")]
+    [ApiController]
+    public class ShowMeTheCodeController: ControllerBase
+    {
+        /// <summary>
+        /// Retornar a url onde está o código fonte no github.
+        /// </summary>
+        /// <response code="200">Github Url</response>
+        [HttpGet("showmethecode")]
+        [ProducesResponseType(200)]
+        public IActionResult Get()
+        {
+            return Ok("https://github.com/luisfernandomoraes/CalculateInterestSolution");
+        }
+    }
+}
